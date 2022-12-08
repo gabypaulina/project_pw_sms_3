91c9f8d52ecfc17ce8cf0f6c088dd5a7dadf663b
gabypaulina <gabypaulina90@gmail.com>
2022-12-09 01:01:32 +0700

    fix db auth

--- hub.php
+++ hub.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    $conn = mysqli_connect("localhost", "root", "", "rawrcake_db");
+    $conn = mysqli_connect("localhost", "rawrcake", "_6v46r(eFE(H8aT9wN", "rawrcake_db");
 
     if(mysqli_connect_errno()){
         echo mysqli_error($conn);
